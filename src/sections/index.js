81324e98088ff2fca5b2f69e8b3e36fbf7b42687
cjephuneh <calebjephuneh@gmail.com>
2023-08-19 22:42:13 +0300

    index.js file

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe.jsx"
+import SpecialOffer from './SpecialOffer'
+
+
+export {
+    Hero,
+    Footer ,
+    Services,
+    PopularProducts,
+    CustomerReviews,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer
+};
